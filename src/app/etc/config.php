ecf2419edced44b2efba215cc8185e1e2fd48f84
M2Panda-courses <panda1445@rambler.ru>
2023-09-19 16:08:10 +0300

    task 7 PDP - Layout - First Store

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -351,7 +351,9 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Aus_Task3' => 1,
+        'Aus_Task4' => 1,
         'Aus_Task5' => 1,
+        'Aus_Task7' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1
