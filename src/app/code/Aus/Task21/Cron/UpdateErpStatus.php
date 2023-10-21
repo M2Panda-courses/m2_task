9bd20475124a2dd6d8dfe63404398aee51e6e3fa
M2Panda-courses <panda1445@rambler.ru>
2023-10-21 16:42:13 +0300

    fix task 21 finished export cron

--- src/app/code/Aus/Task21/Cron/UpdateErpStatus.php
+++ src/app/code/Aus/Task21/Cron/UpdateErpStatus.php
@@ -1,41 +1,5 @@
 <?php
 namespace Aus\Task21\Cron;
-//
-//use Magento\Framework\Api\SearchCriteriaBuilder;
-//use Psr\Log\LoggerInterface;
-//use Aus\Task13\Model\ResourceModel\Exported\Collection as ExportedCollection;
-//class UpdateErpStatus
-//{
-//    /**
-//     * @var \Magento\Catalog\Api\ProductAttributeRepositoryInterface
-//     */
-//    protected $exportedCollection;
-//
-//
-//    protected $logger;
-//
-//    public function __construct(
-//        LoggerInterface $logger,
-//        ExportedCollection $exportedCollection,
-//    ) {
-//        $this->exportedCollection = $exportedCollection;
-//        $this->logger = $logger;
-//    }
-//
-//    public function execute()
-//    {
-//        $this->logger->info('Cron Order Export Started');
-//        $exportOrderById = [];
-//
-//        $this->exportedCollection->addFieldToFilter('exported', '0');
-//        foreach ($this->exportedCollection as $item){
-//            $exportOrderById[] = $item->getOrderId();
-//        }
-//
-//        $this->logger->info('Cron Order Export Finished');
-//    }
-//}
-
 
 use Magento\Sales\Model\Order;
 
@@ -57,10 +21,8 @@ class UpdateErpStatus
         $orders = $this->getNonExportedOrders();
 
         foreach ($orders as $order) {
-            // Симуляція експорту
             $this->simulateExport($order);
 
-            // Позначення замовлення як експортоване
             $order->setErpStatus('Success');
             $order->save();
         }
